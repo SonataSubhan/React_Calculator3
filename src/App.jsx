64a33ee30e60390fb77448dae2013d16c8069390
Subhan Huseynov <johndoe@email.com>
2025-07-11 17:27:27 +0400

    v02

--- src/App.jsx
+++ src/App.jsx
@@ -36,20 +36,57 @@ function reducer(state, { type, payload }) {
         currentOperand: `${state.currentOperand || ''}${payload.digit}`
       }
     case ACTIONS.CHOOSE_OPERATION:
+      if (state.currentOperand == null && state.previousOperand == null) {
+        return state
+      }
+      if (state.currentOperand == null) {
+        return ({
+          ...state,
+          operation: payload.operation
+        })
+      }
+      if (state.previousOperand == null) {
+        return {
+          ...state,
+          operation: payload.operation,
+          previousOperand: state.currentOperand,
+          currentOperand: null
+        }
+      }
       return {
         ...state,
+        previousOperand: evaluate(state),
         operation: payload.operation,
-        previousOperand: state.currentOperand,
         currentOperand: null
       }
     case ACTIONS.CLEAR:
       return {}
     case ACTIONS.DELETE_DIGIT:
+      if (state.overwrite) {
+        return {
+          ...state,
+          currentOperand: null,
+          overwrite: false
+        }
+
+      }
+      if (state.currentOperand == null) {
+        return state
+      }
+      if (state.currentOperand.length === 1) {
+        return {
+          ...state,
+          currentOperand: null
+        }
+      }
       return {
         ...state,
         currentOperand: state.currentOperand.slice(0, -1)
       }
     case ACTIONS.EVALUATE:
+      if (state.operation == null || state.currentOperand == null || state.previousOperand == null) {
+        return state
+      }
       return {
         ...state,
         overwrite: true,
